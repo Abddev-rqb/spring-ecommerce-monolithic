656e1cb2a03cfc6313bf1a409947a81cc4891578
“Abddev-rqb” <abdulch530@gmail.com>
2025-02-03 14:44:00 +0530

    Create controller get and post methods

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,4 +1,24 @@
 package com.ecommerce.project.controller;
 
+
+import com.ecommerce.project.model.Category;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class CategoryController {
+    private List<Category> categories = new ArrayList<>();
+
+    @GetMapping("api/public/categories")
+    public List<Category> getAllCategories(){
+        return categories;
+    }
+
+    @PostMapping("api/admin/category")
+    public String addCategory(@RequestBody Category category){
+        categories.add(category);
+        return "Created Successfully";
+    }
 }
