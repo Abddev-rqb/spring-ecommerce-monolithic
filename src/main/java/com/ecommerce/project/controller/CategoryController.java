517a2ec8701e65ea0f5a03d339733de64d9286a2
“Abddev-rqb” <abdulch530@gmail.com>
2025-02-03 16:28:24 +0530

    Create CategoryService implementation

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,6 +2,8 @@ package com.ecommerce.project.controller;
 
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,16 +11,19 @@ import java.util.List;
 
 @RestController
 public class CategoryController {
-    private List<Category> categories = new ArrayList<>();
+
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping("api/public/categories")
     public List<Category> getAllCategories(){
-        return categories;
+        return categoryService.getAllCategories();
     }
 
     @PostMapping("api/admin/category")
     public String addCategory(@RequestBody Category category){
-        categories.add(category);
+        categoryService.addCategory(category);
         return "Created Successfully";
+
     }
 }
