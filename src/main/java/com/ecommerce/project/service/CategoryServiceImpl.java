9c09ecfc67e3ca655a890fa58730885d880ba2d8
“Abddev-rqb” <abdulch530@gmail.com>
2025-02-13 09:46:43 +0530

    Generate value for DB Id
    Create an annotation of generated value with strategy type of Identity,
    remove the auto generation code line from the service implementaiton file.

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -12,7 +12,6 @@ import java.util.Optional;
 @Service
 public class CategoryServiceImpl implements CategoryService{
     private List<Category> categories = new ArrayList<>();
-    private Long nextId = 1L;
 
     @Override
     public List<Category> getAllCategories(){
@@ -21,7 +20,6 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void addCategory(Category category) {
-        category.setCategoryId(nextId++);
         categories.add(category);
     }
 
