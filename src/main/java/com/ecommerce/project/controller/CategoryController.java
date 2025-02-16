dbc35ec921fa14d35e90ea80b296e60a6fc73d0d
“Abddev-rqb” <abdulch530@gmail.com>
2025-02-16 16:19:20 +0530

    Clean controller, Add valid annotation in update category

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -7,9 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @RestController
@@ -33,16 +32,12 @@ public class CategoryController {
 
     @DeleteMapping("admin/category/{id}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long id){
-        try{
             String status = categoryService.deleteCategory(id);
             return new ResponseEntity<>(status, HttpStatus.OK);
-        }catch (ResponseStatusException e){
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
     }
 
     @PutMapping("admin/category/{id}")
-    public ResponseEntity<Category> updateCategory(@RequestBody Category category, @PathVariable Long id){
+    public ResponseEntity<Category> updateCategory(@Valid @RequestBody Category category, @PathVariable Long id){
             Category status = categoryService.updateCategory(category, id);
             return new ResponseEntity<>(status, HttpStatus.OK);
     }
