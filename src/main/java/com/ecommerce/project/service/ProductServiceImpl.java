27429e9b17b7b1cc16eb6848a311a77714758db9
“Abddev-rqb” <abdulch530@gmail.com>
2025-03-03 17:29:04 +0530

    Promote loose coupling by adding file service
    Create file service, file service implementation in the service folder,
    Override the file conversion i.e., uploadImage method from the file service which is promoted to loose coupling from the product service implementation file,
    Take out the folder path and execute it in the app properties and use value annotation to retrieve the folder name to the product service implementation file.

--- src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -10,6 +10,7 @@ import com.ecommerce.project.repositories.CategoryRepository;
 import com.ecommerce.project.repositories.ProductRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -30,6 +31,11 @@ public class ProductServiceImpl implements ProductService{
     private ProductRepository productRepository;
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private FileService fileService;
+
+    @Value("${project.image}")
+    private String path;
 
     @Override
     public ProductDTO addProduct(Long categoryId, ProductDTO productDTO){
@@ -94,25 +100,11 @@ public class ProductServiceImpl implements ProductService{
     @Override
     public ProductDTO updateImage(Long productId, MultipartFile image) throws IOException {
         Product product = productRepository.findById(productId).orElseThrow(() -> new ResourceNotFoundException("Product", "productId", productId));
-        String path = "images/";
-        String fileName = uploadImage(path, image);
+        String fileName = fileService.uploadImage(path, image);
 
         product.setImage(fileName);
         Product updateProImg = productRepository.save(product);
         return modelMapper.map(updateProImg, ProductDTO.class);
-    }
-
-    private String uploadImage(String path, MultipartFile file) throws IOException{
-        String originalFileName = file.getOriginalFilename();
-        String randomId = UUID.randomUUID().toString();
-        String fileName = randomId.concat(originalFileName.substring(originalFileName.lastIndexOf('.')));
-        String filePath = path + File.separator + fileName;
-        File folder = new File(path);
-        if(!folder.exists()){
-            folder.mkdir();
-        }
-        Files.copy(file.getInputStream(), Paths.get(filePath));
-        return fileName;
     }
 
 }
