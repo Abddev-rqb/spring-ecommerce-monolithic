51723939e8cd60b64336ef0b5f7fabe96e0bf250
“Abddev-rqb” <abdulch530@gmail.com>
2025-03-12 12:25:05 +0530

    Manage order placements
    Create a order controller in the controller folder and autowired it to the order service file,
    Find the cart by emailId, write a null pointer exception to the cart,
    Find the addressId and store it in a variable,
    Create a new order and set the order inputs,
    Use the payment gateway constructor and set the updated order then save to payment repository,
    Along with that set the payment details to updated order and save the order to order repository,
    Get the cart items quantity and reduce the product's quantity then save the changes to the product repository,
    Once order is placed delete the product from cart,
    Convert the saved order into the order DTO each order item converted into orderItem DTO and set the addressId then return the orderDTO.

--- src/main/java/com/ecommerce/project/service/CartServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CartServiceImpl.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
@@ -79,15 +80,19 @@ public class CartServiceImpl implements CartService{
     public List<CartDTO> getAllCarts() {
         List<Cart> carts = cartRepository.findAll();
 
-        if (carts.size() == 0) {
+        if (carts.isEmpty()) {
             throw new APIException("No cart exists");
         }
 
         List<CartDTO> cartDTOs = carts.stream().map(cart -> {
             CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
 
-            List<ProductDTO> products = cart.getCartItems().stream()
-                    .map(p -> modelMapper.map(p.getProduct(), ProductDTO.class)).toList();
+            List<ProductDTO> products = cart.getCartItems().stream().map(cartItem -> {
+                ProductDTO productDTO = modelMapper.map(cartItem.getProduct(), ProductDTO.class);
+                productDTO.setQuantity(cartItem.getQuantity()); // Set the quantity from CartItem
+                return productDTO;
+            }).toList();
+
 
             cartDTO.setProductDTO(products);
 
