2e74ad60d832ea6339a6ed687a32b3604dff5418
“Abddev-rqb” <abdulch530@gmail.com>
2025-03-21 14:23:08 +0530

    Image MVC Configurer update
    Typo error while delcaring the image file

--- src/main/java/com/ecommerce/project/config/WebMvcConfig.java
+++ src/main/java/com/ecommerce/project/config/WebMvcConfig.java
@@ -5,9 +5,9 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvdConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**").addResourceLocations("files:images");
+        registry.addResourceHandler("/images/**").addResourceLocations("file:images/");
     }
 }
